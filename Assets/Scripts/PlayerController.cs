aa4890a8305a87286f3837f6baeda2c37e680359
Rasmus Selsmark <rasmus@unity3d.com>
2017-05-02 21:47:35 +0300

    Added mobile joystick controller from Standard Assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 [System.Serializable]
 public class Boundary
@@ -16,12 +17,12 @@ public class PlayerController : MonoBehaviour
     public GameObject shot;
     public Transform shotSpawn;
     public float fireRate;
-	 
+
     private float nextFire;
 
     void Update ()
     {
-        if (Input.GetButton ("Fire1") && Time.time > nextFire)
+        if (CrossPlatformInputManager.GetButton ("Fire") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
@@ -31,18 +32,18 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate ()
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = CrossPlatformInputManager.GetAxis ("Horizontal");
+        float moveVertical = CrossPlatformInputManager.GetAxis ("Vertical");
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
         GetComponent<Rigidbody> ().velocity = movement * speed;
-		
+
         GetComponent<Rigidbody> ().position = new Vector3 (
             Mathf.Clamp (GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax), 
             0.0f, 
             Mathf.Clamp (GetComponent<Rigidbody> ().position.z, boundary.zMin, boundary.zMax)
         );
-		
+
         GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * -tilt);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Space Shooter
 
 Cleaned version of Space Shooter tutorial project from https://www.assetstore.unity3d.com/en/#!/content/13866
+
+Maintained with Unity 5.6.0f3
